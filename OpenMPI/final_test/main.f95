e40049877c233164fd7804720687bce40c384dd7
Askaniy <askaniy1625@gmail.com>
2023-04-19 22:59:07 +0300

    MPI без отладки

--- OpenMPI/final_test/main.f95
+++ OpenMPI/final_test/main.f95
@@ -9,7 +9,7 @@ program MPIfinaltest
 
     integer(4) :: n, i, j, b, band_num, band_len, step
 	integer, parameter :: k=10, max_step=100 ! шаг итераций при записи в файл и максимальное количество шагов
-    real(mp), allocatable :: qh2(:,:), qh2_crop(:,:), u0(:,:), u0_crop(:,:), u1(:,:), u1_crop(:,:)
+    real(mp), allocatable :: qh2(:,:), qh2_crop(:,:), u(:,:), u0_crop(:,:), u1_crop(:,:)
     
     call mpi_init(err)
     call mpi_comm_size(mpi_comm_world, nproc, err)
@@ -21,15 +21,12 @@ program MPIfinaltest
 		open(1, file='sources.dat', status='old')
 		    read(1,'(2x, i5)') n
 		    allocate(qh2(n, n))
-		    allocate(u0(0:n+1, 0:n+1))
-		    allocate(u1(0:n+1, 0:n+1))
-			qh2 = 0
-		    read(1,*) qh2(1:n, 1:n)
+		    allocate(u(n, n))
+		    read(1,*) qh2
 			band_len = n / band_num
 		close(1)
-		u0 = 0
-		call save_u(u0, 0)
-		u1 = 0
+		u = 0
+		call save_u(u, 0)
 		! отправляем базовые параметры и qh2_crop
 		do b=1,band_num
 			call mpi_send(band_len, 1, mpi_integer4, b, 100, mpi_comm_world, err)
@@ -53,17 +50,26 @@ program MPIfinaltest
 	do step=1,max_step
 		if (myid == 0) then
 			if (mod(step, k) == 0) then
-				! *собираем поля в u1*
-				call save_u(u1, step)
+				! собираем поля в u1
+				do b=1,band_num
+					call mpi_recv(u(band_len*(b-1):band_len*b, :), band_len*n, mpi_real4, b, 999, mpi_comm_world, status, err)
+				end do
+				call save_u(u, step)
 			end if
 		else
-			! *получаем и записываем границы*
-			if (myid == 1) then
-				! получаем только от myid=2
-			else if (myid == band_num)
-				! получаем только от myid=band_num-1
-			else
-				! получаем от myid-1 и myid+1
+			if (step /= 1) then
+				! получаем и записываем границы
+				if (myid == 1) then
+					! получаем только от id=2
+					call mpi_recv(u0_crop(band_len+1, :), n, mpi_real4, 2, 009, mpi_comm_world, status, err)
+				else if (myid == band_num)
+					! получаем только от id=band_num-1
+					call mpi_recv(u0_crop(n-band_len, :), n, mpi_real4, band_num-1, 900, mpi_comm_world, status, err)
+				else
+					! получаем от myid-1 и myid+1
+					call mpi_recv(u0_crop((myid-1)*band_len, :), n, mpi_real4, myid-1, 901, mpi_comm_world, status, err)
+					call mpi_recv(u0_crop(myid*band_len+1, :), n, mpi_real4, myid+1, 109, mpi_comm_world, status, err)
+				end if
 			end if
 
 			forall (i=1:band_len, j=1:n)
@@ -71,17 +77,21 @@ program MPIfinaltest
 			end forall
 
 			if (mod(step, k) == 0) then
-				! *отправляем поле*
+				! отправляем поле
+				call mpi_recv(u1_crop(1:band_len, 1:n), band_len*n, mpi_real4, 0, 999, mpi_comm_world, err)
 			end if
 
 			if (step /= max_step) then
-				! *отправляем границы соседним полям*
+				! отправляем границы соседним полям
 				if (myid == 1) then
-					! отправляем только myid=2
+					! отправляем только id=2
+					call mpi_send(u1_crop(band_len+1, :), n, mpi_real4, 2, 009, mpi_comm_world, err)
 				else if (myid == band_num)
-					! отправляем только myid=band_num-1
+					! отправляем только id=band_num-1
+					call mpi_send(u1_crop(n-band_len, :), n, mpi_real4, band_num-1, 900, mpi_comm_world, err)
 				else
-					! отправляем myid-1 и myid+1
+					! отправляем id-1 и id+1
+					call mpi_send(u1_crop(myid*band_len+1, :), n, mpi_real4, myid+1, 109, mpi_comm_world, err)
 				end if
 			end if
 
@@ -98,9 +108,9 @@ program MPIfinaltest
 		integer :: m, indx, row
 		m = size(u, dim=2)
 		open(1, file='data/data'//str(indx)//'.dat')
-			write(1,'("# ", i0)') m-2
-			do row = 2,m-1
-				write(1,'('//str(m)//'(f10.'//str(dp)//'))') u(2:m-1, row)
+			write(1,'("# ", i0)') m
+			do row = 1,m
+				write(1,'('//str(m)//'(f10.'//str(dp)//'))') u(: row)
 			end do
 		close(1)
 	end subroutine
--- OpenMPI/test2/main.f95
+++ OpenMPI/test2/main.f95
@@ -4,8 +4,8 @@ program MPItest2
 	use my_math
     implicit none
     
-    integer(4) :: err, myid, nproc, band_num
-    real(mp) :: arr(3)
+    integer(4) :: err, myid, nproc, band_num, b
+    real(mp) :: arr(3), arrr(3,3)
     integer, dimension(mpi_status_size) :: status
 
     call mpi_init(err)
@@ -17,12 +17,27 @@ program MPItest2
 
     if (myid == 0) then
         !write(*,*) 'myid 0'
-        arr = [1, 2, 3]
+        arr = [0, 0, 0]
         call output('0: arr =', arr)
-        call mpi_send(arr, size(arr), mpi_real4, 1, 666, mpi_comm_world, err)
+        !call mpi_send(arr, size(arr), mpi_real4, 1, 666, mpi_comm_world, err)
+        !call mpi_gather(SBuf, SCount, SType, RBuf, RCount, RType, Root, Comm, Err)
+        do b=1,band_num
+            call mpi_recv(arrr(:,b), size(arr), mpi_real4, b, 666, mpi_comm_world, status, err)
+        end do
+        call output('0: arrr =', arrr)
     else if (myid == 1) then
-        call mpi_recv(arr, size(arr), mpi_real4, 0, 666, mpi_comm_world, status, err)
-        call output(str(myid)//'/'//str(band_num)//': arr =', arr)
+        !call mpi_recv(arr, size(arr), mpi_real4, 0, 666, mpi_comm_world, status, err)
+        arr = [1, 1, 1]
+        !call output(str(myid)//'/'//str(band_num)//': arr =', arr)
+        call mpi_send(arr, size(arr), mpi_real4, 0, 666, mpi_comm_world, err)
+    else if (myid == 2) then
+        arr = [2, 2, 2]
+        !call output(str(myid)//'/'//str(band_num)//': arr =', arr)
+        call mpi_send(arr, size(arr), mpi_real4, 0, 666, mpi_comm_world, err)
+    else if (myid == 3) then
+        arr = [3, 3, 3]
+        !call output(str(myid)//'/'//str(band_num)//': arr =', arr)
+        call mpi_send(arr, size(arr), mpi_real4, 0, 666, mpi_comm_world, err)
     end if
 
     !write(*,*) 'Hi'
