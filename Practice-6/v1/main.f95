ee473df5206c4f1e2445dc569bab07b389da787b
Askaniy <askaniy1625@gmail.com>
2023-10-10 21:05:54 +0300

    Шаблон 6-го задания

--- Practice-6/v1/main.f95
+++ Practice-6/v1/main.f95
@@ -1,6 +1,6 @@
-! Задание N: 
+! Задание 6: Многомерный метод Ньютона
 
-program questNvN
+program quest6v1
     use my_io
     use my_math
     implicit none
@@ -18,5 +18,15 @@ program questNvN
     open(1, file='result.dat')
         write(1,'(2f9.'//str(dp)//')') array
     close(1)
+
+    contains
+
+    function f(x):
+        real(mp), intent(in) :: x(:)
+        real(mp) :: f(size(n))
+        do concurrent (i=1,size(n))
+            f(i) = x**i - 2
+        end do
+    end function
     
 end program
