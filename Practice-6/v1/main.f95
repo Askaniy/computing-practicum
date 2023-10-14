fda87a10941bc705ec32eeac3d77e07a7dc64c90
Askaniy <44873271+Askaniy@users.noreply.github.com>
2023-10-14 11:10:42 +0300

    Тестирующая программа и 3D визуализация

--- Practice-6/v1/main.f95
+++ Practice-6/v1/main.f95
@@ -6,27 +6,34 @@ program quest6v1
     implicit none
     
     integer :: i, j, n
-    real(mp), allocatable :: array(:,:)
+    real(mp), allocatable :: solution(:)
 
-    open(1, file='data.dat', status='old')
-        read(1,'(2x, i5)') n
-        allocate(array(2, n+1))
-        read(1,*) array
-    close(1)
-    call output('array =', array)
+    solution = [1, 2, 3]
 
     open(1, file='result.dat')
-        write(1,'(2f9.'//str(dp)//')') array
+        write(1,'(f9.'//str(dp)//')') solution
     close(1)
 
     contains
 
-    function f(x):
+    function test_func_1(x)
         real(mp), intent(in) :: x(:)
-        real(mp) :: f(size(n))
-        do concurrent (i=1,size(n))
-            f(i) = x**i - 2
+        real(mp) :: test_func_1(size(x))
+        do concurrent (i=1:size(x))
+            test_func_1(i) = x(i)**i - 2 ! решения: (2, +-2^(1/2), 2^(1/3))
         end do
+    end function
+
+    function test_func_2(x)
+        real(mp), intent(in) :: x(:)
+        real(mp) :: test_func_2(size(x)), matrix(size(x), size(x))
+        n = size(x)
+        ! генерация матрицы, однозначно определяемой размерностью
+        do concurrent (i=1:n, j=1:n)
+            matrix(i,j) = mod(i+j, n) + i - j
+        end do
+        matrix(:,n-1) = matrix(:,n) ! уменьшение ранга для возникновения нетривиального решения
+        test_func_2 = multiply(matrix, x)
     end function
     
 end program
---
+++ Practice-6/v1/plot.py
@@ -0,0 +1,54 @@
+import warnings
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Скопированные настройки из другого проекта, TCT
+
+# Filling empty space on a plot
+warnings.simplefilter('ignore', UserWarning)
+plt.rcParams['figure.autolayout'] = True
+
+# MatPlotLib custom theme
+# https://matplotlib.org/stable/tutorials/introductory/customizing.html
+text_color = '#FFFFFF'
+muted_color = '#A3A3A3'
+highlight_color = '#5A5A5A'
+bg_color = '#333333'
+inputON_color = '#424242'
+plt.rcParams |= {
+    'text.color': text_color, 'axes.labelcolor': text_color,
+    'axes.edgecolor': muted_color, 'xtick.color': muted_color, 'ytick.color': muted_color,
+    'figure.facecolor': bg_color, 'axes.facecolor': bg_color,
+    'axes.grid': True, 'grid.color': highlight_color
+    }
+
+def test_func_1(x, y, z):
+    return np.array([x, y*y, z*z*z]) - 2
+
+def test_func_2(x, y, z):
+    return np.array([2*x+y+3*z, -x+y, -x+y])
+
+solution = np.loadtxt('result.dat')
+
+linspace = np.linspace(-10, 10, 10)
+x, y, z = np.meshgrid(linspace, linspace, linspace)
+u, v, w = test_func_2(x, y, z)
+lengths = np.sqrt(u**2 + v**2 + w**2)
+lengths /= np.max(lengths)
+lengths = lengths.flatten()
+colors = np.zeros((lengths.size, 4))
+colors[:,0] = lengths # red
+colors[:,1] = lengths / 5 # green
+colors[:,3] = lengths # alpha
+
+fig = plt.figure(figsize=(10, 10), dpi=100)
+ax = fig.add_subplot(111, projection='3d')
+ax.quiver(x, y, z, u, v, w, length=2, normalize=True, color=colors, label='Векторное поле')
+ax.plot(linspace, linspace, -linspace, color='#108BB4', label='Линия решений')
+ax.scatter3D(*solution, color='#00FF00', label='Решение')
+ax.set_xlabel('X axis')
+ax.set_ylabel('Y axis')
+ax.set_zlabel('Z axis')
+ax.legend()
+fig.savefig('plot.png', dpi=100)
+#fig.show()
